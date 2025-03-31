1a86973f6b526b6b9fecc58bd3ce2f9fc5e7171f
Jayaprakash Panabaka <jpanabaka1729@gmail.com>
2025-03-31 18:32:35 +0530

    changes

--- src/Module 3/Introduction to ReactJS/Ques_5.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_5.jsx
@@ -1,5 +1,5 @@
 // Passing Props to Components
 // Description: Create a Profile component that receives name and age as props and displays them.
 
-// Step 1: Create src/Profile.js file and define the Profile component
+// Step 1: Write your code within the file, by the name of component as Profile
 // Step 2: Update App.js to pass props to the Profile component
--- src/Module 3/Introduction to ReactJS/Ques_6.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_6.jsx
@@ -1,6 +1,6 @@
 // Conditional Rendering
 // Description: Implement a component that renders different messages based on a passed isLoggedIn prop.
 
-// Step 1: Create src/LoginMessage.js file
+// Step 1: Write your code within the file, by the name of component as LoginMessage
 // Step 2: Modify the App.js to use LoginMessage with a boolean prop
 
