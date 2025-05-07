42899651eee93d2ad14d08227392068354f3f86a
yaravacharanreddy <151006293+yaravacharanreddy@users.noreply.github.com>
2025-05-07 08:20:42 +0000

    completed

--- src/Module 3/Introduction to ReactJS/Ques_5.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_5.jsx
@@ -2,4 +2,15 @@
 // Description: Create a Profile component that receives name and age as props and displays them.
 
 // Step 1: Write your code within the file, by the name of component as Profile
-// Step 2: Update App.js to pass props to the Profile component
+// Step 2: Update App.js to pass props to the Profile componentimport React from 'react';
+
+const Profile = ({ name, age }) => {
+  return (
+    <div>
+      <h1>Name: {name}</h1>
+      <p>Age: {age}</p>
+    </div>
+  );
+}
+
+export default Profile;
