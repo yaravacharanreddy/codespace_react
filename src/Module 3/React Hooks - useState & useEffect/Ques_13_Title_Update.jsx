18f2e6b5f27530c4e6b2e422ed9a26c84789a57b
yaravacharanreddy <151006293+yaravacharanreddy@users.noreply.github.com>
2025-05-07 09:10:43 +0000

    completed

--- src/Module 3/React Hooks - useState & useEffect/Ques_13_Title_Update.jsx
+++ src/Module 3/React Hooks - useState & useEffect/Ques_13_Title_Update.jsx
@@ -6,3 +6,26 @@
 //     - Use useEffect to update the document title whenever the count changes.
 //     - Create a button to increase the count and see the document title change.
 //     - Write your code within the file, by the name of component as Title_Update
+import React, { useState, useEffect } from 'react';
+
+const TitleUpdate = () => {
+  const [clicks, setClicks] = useState(0);
+
+  useEffect(() => {
+    document.title = `Button clicked ${clicks} times`;
+  }, [clicks]);
+
+  const handleClick = () => {
+    setClicks(prevClicks => prevClicks + 1);
+  };
+
+  return (
+    <div>
+      <h2>Click the Button</h2>
+      <button onClick={handleClick}>Click me</button>
+      <p>Button clicked {clicks} times</p>
+    </div>
+  );
+};
+
+export default TitleUpdate;
