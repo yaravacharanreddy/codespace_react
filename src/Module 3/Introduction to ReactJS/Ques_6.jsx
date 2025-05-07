96b5a4be76c3152fbe19c41462bf96325a0ba909
yaravacharanreddy <151006293+yaravacharanreddy@users.noreply.github.com>
2025-05-07 08:26:50 +0000

    completed

--- src/Module 3/Introduction to ReactJS/Ques_6.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_6.jsx
@@ -1,6 +1,11 @@
-// Conditional Rendering
-// Description: Implement a component that renders different messages based on a passed isLoggedIn prop.
+import React from 'react';
 
-// Step 1: Write your code within the file, by the name of component as LoginMessage
-// Step 2: Modify the App.js to use LoginMessage with a boolean prop
+const ConditionalRendering = ({ isLoggedIn }) => {
+  return (
+    <div>
+      {isLoggedIn ? <h1>Welcome back!</h1> : <h1>Please log in.</h1>}
+    </div>
+  );
+}
 
+export default ConditionalRendering;
