fc92f9eed40a318edffba35628bd26afaa969213
yaravacharanreddy <151006293+yaravacharanreddy@users.noreply.github.com>
2025-05-07 08:32:05 +0000

    completed

--- src/Module 3/React Hooks - useState & useEffect/Ques_1_Counter.jsx
+++ src/Module 3/React Hooks - useState & useEffect/Ques_1_Counter.jsx
@@ -7,3 +7,21 @@
 //     - Write your code within the file, by the name of component as Counter
 
 
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={incrementCount}>Increment</button>
+    </div>
+  );
+}
+
+export default Counter;
