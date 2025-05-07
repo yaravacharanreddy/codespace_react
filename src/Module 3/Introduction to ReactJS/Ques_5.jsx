8eac7d3783c89be5694bbb78c9b1c3e938a0dc91
yaravacharanreddy <151006293+yaravacharanreddy@users.noreply.github.com>
2025-05-07 08:22:26 +0000

    completed

--- src/Module 3/Introduction to ReactJS/Ques_5.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_5.jsx
@@ -4,6 +4,9 @@
 // Step 1: Write your code within the file, by the name of component as Profile
 // Step 2: Update App.js to pass props to the Profile componentimport React from 'react';
 
+import React from 'react';
+import PropTypes from 'prop-types';
+
 const Profile = ({ name, age }) => {
   return (
     <div>
@@ -12,5 +15,15 @@ const Profile = ({ name, age }) => {
     </div>
   );
 }
+
+Profile.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number
+};
+
+Profile.defaultProps = {
+  name: 'Unknown',
+  age: 'Not specified'
+};
 
 export default Profile;
