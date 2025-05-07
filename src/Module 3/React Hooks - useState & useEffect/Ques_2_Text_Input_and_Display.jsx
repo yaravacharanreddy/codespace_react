39d11008e981ff4400041f5e80e6488be2cfc28f
yaravacharanreddy <151006293+yaravacharanreddy@users.noreply.github.com>
2025-05-07 08:34:03 +0000

    completed

--- src/Module 3/React Hooks - useState & useEffect/Ques_2_Text_Input_and_Display.jsx
+++ src/Module 3/React Hooks - useState & useEffect/Ques_2_Text_Input_and_Display.jsx
@@ -3,3 +3,26 @@
 
 // Steps:
 //     - Write your code within the file, by the name of component as Text_Input
+import React, { useState } from 'react';
+
+const TextInputDisplay = () => {
+  const [inputText, setInputText] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input 
+        type="text" 
+        value={inputText} 
+        onChange={handleInputChange} 
+        placeholder="Type something"
+      />
+      <p>{inputText}</p>
+    </div>
+  );
+}
+
+export default TextInputDisplay;
