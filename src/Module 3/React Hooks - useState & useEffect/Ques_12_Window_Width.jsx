7c4f186850391c8a47da5eeb783080c833d95cc0
yaravacharanreddy <151006293+yaravacharanreddy@users.noreply.github.com>
2025-05-07 09:07:49 +0000

    completed

--- src/Module 3/React Hooks - useState & useEffect/Ques_12_Window_Width.jsx
+++ src/Module 3/React Hooks - useState & useEffect/Ques_12_Window_Width.jsx
@@ -1,15 +1,7 @@
-// Window Width
-
-// 1. Goal: Track and display the window width as the window resizes.
-// 2. Steps:
-//     - Use useEffect to add an event listener for the window resize event.
-//     - Use useState to store the current window width.
-//     - Update the width state whenever the window is resized and display it.
-//     - Write your code within the file, by the name of component as Window_Width
 import React, { useState, useEffect } from 'react';
 
 const WindowWidth = () => {
-  const [width, setWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(() => window.innerWidth);
 
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
